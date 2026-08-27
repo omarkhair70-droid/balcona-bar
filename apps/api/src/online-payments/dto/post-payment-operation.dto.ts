2ed55ada449b9dfee24a732dcdd6b94c77a1b3b9
omarkhair70-droid <omar.khair70@gmail.com>
2026-08-28 00:47:40 +0300

    feat(payments): add refund void capture DTOs

---
+++ apps/api/src/online-payments/dto/post-payment-operation.dto.ts
@@ -0,0 +1,48 @@
+import {
+  IsInt,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from "class-validator";
+
+export class RefundOnlinePaymentDto {
+  @IsInt()
+  @Min(1)
+  amountMinor!: number;
+
+  @IsString()
+  @MaxLength(120)
+  idempotencyKey!: string;
+
+  @IsString()
+  @MaxLength(500)
+  @IsOptional()
+  reason?: string;
+}
+
+export class VoidOnlinePaymentDto {
+  @IsString()
+  @MaxLength(120)
+  idempotencyKey!: string;
+
+  @IsString()
+  @MaxLength(500)
+  @IsOptional()
+  reason?: string;
+}
+
+export class CaptureOnlinePaymentDto {
+  @IsInt()
+  @Min(1)
+  amountMinor!: number;
+
+  @IsString()
+  @MaxLength(120)
+  idempotencyKey!: string;
+
+  @IsString()
+  @MaxLength(500)
+  @IsOptional()
+  reason?: string;
+}
