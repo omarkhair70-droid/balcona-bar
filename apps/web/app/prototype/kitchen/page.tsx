cc795373c9e98ed153364c4cc3a3bfae82642099
Omar Khair <omar.khair70@gmail.com>
2026-08-28 11:02:41 +0300

    chore(ux): trigger Kitchen preview deployment

--- apps/web/app/prototype/kitchen/page.tsx
+++ apps/web/app/prototype/kitchen/page.tsx
@@ -1,3 +1,4 @@
+// Kitchen preview checkpoint: complete prototype tree
 import { KitchenPrototype } from "@/features/prototype/kitchen-prototype";
 
 export default function KitchenPrototypePage() {
