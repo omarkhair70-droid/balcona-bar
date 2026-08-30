dd24005c0df85f1dab188900387858f9819dda82
Omar Khair <omar.khair70@gmail.com>
2026-08-30 18:23:32 +0300

    feat(billing): register SaaS billing service

--- apps/api/src/saas/saas.module.ts
+++ apps/api/src/saas/saas.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
+import { AuditModule } from "../audit/audit.module";
 import { PrismaModule } from "../prisma/prisma.module";
 import { StaffModule } from "../staff/staff.module";
 import { SaasController } from "./saas.controller";
+import { SaasBillingService } from "./saas-billing.service";
 import { SaasService } from "./saas.service";
 
 @Module({
-  imports: [PrismaModule, StaffModule],
+  imports: [AuditModule, PrismaModule, StaffModule],
   controllers: [SaasController],
-  providers: [SaasService],
-  exports: [SaasService],
+  providers: [SaasService, SaasBillingService],
+  exports: [SaasService, SaasBillingService],
 })
 export class SaasModule {}
